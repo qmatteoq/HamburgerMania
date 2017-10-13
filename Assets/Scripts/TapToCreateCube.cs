606c2e149074fcde413af1b3e35b132a8a06e0e6
Matteo Pagani <info@qmatteoq.com>
2017-10-13 19:24:27 +0200

    First commit of the project

---
+++ Assets/Scripts/TapToCreateCube.cs
@@ -0,0 +1,41 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using System;
+using HoloToolkit.Unity;
+
+public class TapToCreateCube : MonoBehaviour, IInputClickHandler
+{
+    public Transform Prefab;
+
+    private bool loaded;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        var instance = Instantiate(Prefab);
+        instance.gameObject.transform.position = GazeManager.Instance.GazeOrigin + GazeManager.Instance.GazeNormal * 1.5f;
+        WorldAnchorManager.Instance.AttachAnchor(instance.gameObject, Guid.NewGuid().ToString());
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+    }
+
+    void Update()
+    {
+        if (!this.loaded && (WorldAnchorManager.Instance.AnchorStore != null))
+        {
+            var ids = WorldAnchorManager.Instance.AnchorStore.GetAllIds();
+
+            // NB: I'm assuming that the ordering here is either preserved or
+            // maybe doesn't matter.
+            foreach (var id in ids)
+            {
+                var instance = Instantiate(this.Prefab);
+                WorldAnchorManager.Instance.AttachAnchor(instance.gameObject, id);
+            }
+            this.loaded = true;
+        }
+    }
+}
