2f4277c4f94c7522e760c38d6433cc30a5966391
Matteo Pagani <info@qmatteoq.com>
2017-11-06 20:43:38 +0100

    Updated to the latest version of Mixed Reality Toolkit and MR Design Labs

--- Assets/Scripts/TapToCreateCube.cs
+++ Assets/Scripts/TapToCreateCube.cs
@@ -1,22 +1,15 @@
 ﻿using HoloToolkit.Unity.InputModule;
 using UnityEngine;
-using System;
-using HoloToolkit.Unity;
 
 public class TapToCreateCube : MonoBehaviour, IInputClickHandler
 {
-    public Transform Prefab;
+    public GameObject Prefab;
+    public GameObject Cursor;
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
+        Debug.Log("Clicked");
         var instance = Instantiate(Prefab);
-        instance.gameObject.transform.position = GazeManager.Instance.GazeOrigin + GazeManager.Instance.GazeNormal * 1.5f;
-        WorldAnchorManager.Instance.AttachAnchor(instance.gameObject, Guid.NewGuid().ToString());
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-        InputManager.Instance.PushFallbackInputHandler(gameObject);
+        instance.gameObject.transform.position = Cursor.gameObject.transform.position;
     }
 }
