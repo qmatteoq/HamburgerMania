87c9aeaefb7021066ab87143c55215c9685d95fd
Matteo Pagani <info@qmatteoq.com>
2017-11-10 23:02:14 +0100

    Update of the HoloToolkit

---
+++ Assets/Scripts/ImmersiveManager.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmersiveManager : MonoBehaviour
+{
+    public List<GameObject> Objects;
+    // Use this for initialization
+    void Start()
+    {
+        foreach (var item in Objects)
+        {
+            item.SetActive(!OpaqueDetector.IsOpaque);
+        }
+    }
+}
+
