a7467a728f2066b81604ddefcd2f52808761e717
Matteo Pagani <info@qmatteoq.com>
2017-11-12 16:32:16 +0100

    Added device manager to the project

---
+++ Assets/Scripts/OpaqueDetector.cs
@@ -0,0 +1,20 @@
+﻿namespace Assets.Scripts
+{
+    public static class OpaqueDetector
+    {
+        public static bool IsOpaque
+        {
+            get
+            {
+#if !UNITY_EDITOR && UNITY_UWP
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent(
+                "Windows.Graphics.Holographic.HolographicDisplay"))
+            {
+               return Windows.Graphics.Holographic.HolographicDisplay.GetDefault()?.IsOpaque == true;
+            }
+#endif
+                return false;
+            }
+        }
+    }
+}
